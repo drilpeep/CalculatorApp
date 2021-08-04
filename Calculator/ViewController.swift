4f08008ac9241f3c659a5101326150c7f72194bd
AtomSpace <atomspace@atomspaces-mac-mini.local>
2021-08-04 17:34:04 +0300

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by AtomSpace on 8/4/21.
+//  Copyright © 2021 AtomSpace. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
